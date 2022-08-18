890438f9502837e54a7f955ec41a437430e7e549
Javier Elias Dagobeth Montes <38734266+IngJEDagobethM@users.noreply.github.com>
2022-08-18 13:55:05 -0500

    Trabajando con Flux, just como Observable y Subscribe como Consumer. Se ejemplifica el consumer con error y finalización con éxito.

--- src/main/java/me/ingjedagobethm/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/me/ingjedagobethm/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,43 @@
 package me.ingjedagobethm.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Flux es un Publisher u Observable
+		Flux<String> nombres = Flux.just("Cindy", "Javier", "Adrian", "Fabian", "Duvalier", "Ivonne")
+				//.doOnNext(System.out::println); // de lambda a callable
+				.doOnNext(elemento -> { // Para múltiples instrucciones se coloca entre llaves
+					if(elemento.isEmpty()){
+						throw new RuntimeException("Nombre no puede ser vacio.");
+					}
+					System.out.println(elemento);
+				});
+
+		// Por si solo no es posible "invocar" la ejecución del flujo,
+		// nos debemos subscribir al evento.
+		nombres.subscribe(
+				log::info,
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito.");
+					}
+				}); // Consumer  u Observador
+	}
 }
